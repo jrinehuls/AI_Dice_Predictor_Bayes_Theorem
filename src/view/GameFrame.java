8ce5284fb038e088c642a056b237717d16bbe46d
Justin Rinehuls <jrinehuls@gmail.com>
2024-01-09 21:13:41 -0500

    Add icon image

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -1,12 +1,21 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class GameFrame extends JFrame {
 
     GamePanel gamePanel = new GamePanel();
 
     public GameFrame() {
+        try {
+            this.setIconImage(ImageIO.read(Objects.requireNonNull(this.getClass().getResourceAsStream("/die.png"))));
+        } catch (IOException | NullPointerException e) {
+            this.setIconImage(null);
+        }
         this.add(gamePanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
